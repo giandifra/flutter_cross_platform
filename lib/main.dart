cfc784fd4e9744cd296eed608cbcb6e1fbceba52
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2019-07-17 18:16:42 +0200

    add rc_platform dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'
+    show TargetPlatform, debugDefaultTargetPlatformOverride;
+import 'package:flutter/widgets.dart';
+import 'package:rc_cross_platform/rc_cross_platform.dart';
 
 import 'app.dart';
 
 void main() {
-  if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+  final platform = Platform();
+  //  this is needed at least until https://github.com/flutter/flutter/issues/33881 is fixed
+  if (platform.isDesktop) {
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
+
   runApp(App());
 }
